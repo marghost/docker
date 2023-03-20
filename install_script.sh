6e1c793ddf271776b2a5d7087283948c68103c51
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-03-20 17:57:25 -0400

    Update install_script.sh
    another round of optimisation

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+#If needed to clean previous install
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
 # Install Docker
 sudo apt-get update
-sudo apt-get install -y docker.io
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    apt-transport-https \
+    gnupg-agent
+
+
+#Add docker repo
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add current user to docker group
 sudo usermod -aG docker $(whoami)
