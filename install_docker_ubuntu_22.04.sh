8fa4928fd76a70a739895f3795a33572ed4cb76e
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-03-20 22:01:09 -0400

    Update install_docker_ubuntu_22.04.sh

--- install_docker_ubuntu_22.04.sh
+++ install_docker_ubuntu_22.04.sh
@@ -12,9 +12,11 @@ sudo apt-get install ca-certificate curl gnupg lsb-release apt-transport-https g
 
 #Add docker repo
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
+sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu | $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu 
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 
