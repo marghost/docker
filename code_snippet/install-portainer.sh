1eba06d48d67ec6bab766b559abe08bd7fcd00b3
The Geek Ghost <marghost@protonmail.com>
2023-03-21 22:25:52 -0400

    Create install-portainer.sh

---
+++ code_snippet/install-portainer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install Portainer
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lastest
+echo "Portainer installed"
