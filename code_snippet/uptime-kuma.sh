9f146d5299f4528e55e3dd579543f51280fdad27
The Geek Ghost <marghost@protonmail.com>
2023-03-21 22:54:33 -0400

    Update uptime-kuma.sh

--- code_snippet/uptime-kuma.sh
+++ code_snippet/uptime-kuma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install Uptime Kuma
-sudo docker volume create uptime_kuma_data
-sudo docker run -d -p 3000:3000 --name=uptime-kuma --restart=always -v uptime_kuma_data:/app/data -v /var/run/docker.sock:/var/run/docker.sock uptimekuma/uptime-kuma
+docker volume create uptime_kuma_data
+docker run -d -p 3000:3000 --name=uptime-kuma --restart=always -v uptime_kuma_data:/app/data louislam/uptime-kuma
 echo "Update Kuma installed"
