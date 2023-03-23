8405c6f28c1298033003c144890bce15f5bc996c
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-03-22 22:33:39 -0400

    Update speedtest-tracker.sh

--- code_snippet/speedtest-tracker.sh
+++ code_snippet/speedtest-tracker.sh
@@ -7,3 +7,4 @@ docker run -d -p 8080:80 -p 8443:443 -e PUID=1001 -e PGID=1001 --name=speedtest-
 sleep 15s
 chmod -R 701 /var/lib/docker/volumes/speedtest_tracker_data/_data
 echo "Updated speedtest Tracker installed"
+echo "Dont forget default username : admin@example.com password : password"
