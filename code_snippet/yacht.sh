f903a675e03b479ba0b625a62d0ccce142e550b8
The Geek Ghost <marghost@protonmail.com>
2023-03-23 10:52:37 -0400

    Update yacht.sh

--- code_snippet/yacht.sh
+++ code_snippet/yacht.sh
@@ -1,4 +1,5 @@
 # Install Yacht Docker
 docker volume create yacht_data
 docker run -d -p 8001:8000 --name=yacht --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v yacht_data:/config selfhostedpro/yacht:latest
+echo "Do not forget to change default credentials.  Username : admin@yacht.local password : pass"
 echo "Yacht installed"
