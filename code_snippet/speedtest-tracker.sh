0505a271459c4f954d79bad059026715244fa002
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-03-22 22:14:56 -0400

    Update speedtest-tracker.sh

--- code_snippet/speedtest-tracker.sh
+++ code_snippet/speedtest-tracker.sh
@@ -2,5 +2,7 @@
 
 # Install Speedtest Tracker
 docker volume create speedtest_tracker_data
-docker run -d -p 8080:80 -p 8443:443 -e PUID=1000 -e PGID=1000 --name=speedtest-tracker --restart=always -v speedtest_tracker_data:/app/data ajustesen/speedtest-tracker
+chmod -R 777 ./speedtest_tracker_data
+docker run -d -p 8080:80 -p 8443:443 -e PUID=1001 -e PGID=1001 --name=speedtest-tracker --restart=always -v speedtest_tracker_data:/config ajustesen/speedtest-tracker
+chmod -R 701 ./speedtest_tracker_data
 echo "Good Speedtest Tracker installed"
