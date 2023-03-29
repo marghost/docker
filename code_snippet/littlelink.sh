1b9d1ef7715e17f4c25151f1ee91cf6b5e290614
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-03-29 11:12:35 -0400

    Create littlelink.sh

---
+++ code_snippet/littlelink.sh
@@ -0,0 +1,27 @@
+echo "Please enter your google analytics tracking id"
+read GKEY
+
+docker run -d \
+  --name=littlelink-server \
+  -p 80:3000 \
+  -e META_TITLE='Marghost' \
+  -e META_DESCRIPTION='Marghost Link page' \
+  -e META_AUTHOR='Marghost' \
+  -e LANG=en \
+  -e META_INDEX_STATUS='noindex' \
+  -e THEME='Dark' \
+  -e GA_TRACKING_ID='$GKEY' \
+  -e FAVICON_URL='https://pbs.twimg.com/profile_images/1485603469037875200/qWu5PSZD_200x200.jpg' \
+  -e AVATAR_URL='https://pbs.twimg.com/profile_images/1485603469037875200/qWu5PSZD_200x200.jpg' \
+  -e AVATAR_2X_URL='https://pbs.twimg.com/profile_images/1485603469037875200/qWu5PSZD_400x400.jpg' \
+  -e AVATAR_ALT='Marghost Profile Pic' \
+  -e NAME='Marghost' \
+  -e BIO='Hey! Here are the places where you can connect with me!' \
+  -e WORDPRESS='https://www.thegeekghost.com/' \
+  -e GITHUB='https://github.com/marghost' \
+  -e TWITTER='https://twitter.com/marghost' \
+  -e INSTAGRAM='https://www.instagram.com/marghost/' \
+  -e EMAIL='me@thegeekghost.com' \
+  -e FOOTER=Marghost © 2023 \
+  --restart unless-stopped \
+  ghcr.io/techno-tim/littlelink-server:latest
