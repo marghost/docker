d2f290ff0a5433a4e7691ef44c2e582873870c12
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-03-30 20:47:17 -0400

    Update littlelink.sh

--- code_snippet/littlelink.sh
+++ code_snippet/littlelink.sh
@@ -5,7 +5,7 @@ docker run -d \
   --name=littlelink-server \
   -p 80:3000 \
   -e META_TITLE='Marghost' \
-  -e META_DESCRIPTION='Cybersecurity professionnal | Retro gamer | SMB Thinkerer | Electronic restoration | IoT/IIoT | Homelab | ⚜️ 🍁 | Geek' \
+  -e META_DESCRIPTION='Cybersecurity professionnal | Retro gamer | Electronic Thinkerer | IoT/IIoT | Homelab | ⚜️ 🍁 | Geek' \
   -e META_AUTHOR='Marghost' \
   -e LANG=en \
   -e META_INDEX_STATUS='noindex' \
@@ -17,16 +17,14 @@ docker run -d \
   -e AVATAR_2X_URL='https://pbs.twimg.com/profile_images/1485603469037875200/qWu5PSZD_400x400.jpg' \
   -e AVATAR_ALT='Marghost Profile Pic' \
   -e NAME='Marghost' \
-  -e BIO='Cybersecurity professionnal | Retro gamer | SMB Thinkerer | Electronic restoration | IoT/IIoT | Homelab | ⚜️ 🍁 | Geek' \
-  
+  -e BIO='Cybersecurity professionnal | Retro gamer | Electronic Thinkerer | IoT/IIoT | Homelab | ⚜️ 🍁 | Geek' \
   -e CUSTOM_BUTTON_TEXT='Blog' \
   -e CUSTOM_BUTTON_URL='https://www.thegeekghost.com' \
   -e CUSTOM_BUTTON_COLOR='#000000' \
   -e CUSTOM_BUTTON_TEXT_COLOR='#ffffff' \
   -e CUSTOM_BUTTON_ALT_TEXT='Blog' \
   -e CUSTOM_BUTTON_NAME='BLOG' \
-  -e CUSTOM_BUTTON_ICON='fas file-alt' \ 
-  
+  -e CUSTOM_BUTTON_ICON='fas file-alt' \
   -e GITHUB='https://github.com/marghost' \
   -e TWITTER='https://twitter.com/marghost' \
   -e INSTAGRAM='https://www.instagram.com/marghost/' \
