289a4722a31d78172f4fe6bd8cd4395bcb559f0c
The Geek Ghost <38355186+marghost@users.noreply.github.com>
2023-07-02 14:05:51 -0400

    Update shlink-web-gui.sh

--- code_snippet/shlink-web-gui.sh
+++ code_snippet/shlink-web-gui.sh
@@ -3,4 +3,5 @@ docker run -d \
     -p 8989:80 \
     -e SHLINK_SERVER_URL=<SERVER_URL> \
     -e SHLINK_SERVER_API_KEY=<GENERATED_API_KEY> \
+    --restart unless-stopped \
     shlinkio/shlink-web-client
